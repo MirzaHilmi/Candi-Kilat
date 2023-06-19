112fbf83089a3a8f383d6f345203795da2f86ee2
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-06-19 17:33:18 +0700

    `feat:` Add Roles seeder to Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use App\Models\Author;
 use App\Models\Book;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +18,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Administrative permission
+        Permission::create(['name' => 'publish book']);
+        Permission::create(['name' => 'edit book']);
+        Permission::create(['name' => 'delete book']);
+        // Permission::create(['name' => 'unpublish book']);
+
+        // Guest permission
+        Permission::create(['name' => 'borrow book']);
+        Permission::create(['name' => 'return book']);
+
+        // Create roles and assign created permissions
+
+        $role = Role::create(['name' => 'user']);
+        $role->givePermissionTo(['borrow book', 'return book']);
+
+        $role = Role::create(['name' => 'librarian']);
+        $role->givePermissionTo(Permission::all());
+
         Author::factory()->has(
             Book::factory()->has(
                 Category::factory()->count(3)
