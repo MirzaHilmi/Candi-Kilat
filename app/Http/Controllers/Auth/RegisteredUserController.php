a5eb30a5720cf0548c9c802889ec8cd9a82fd092
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-06-19 17:40:29 +0700

    `feat:` Add Roles for Authenticated User

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,9 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password)
         ]);
 
+        if ($identifierKey == 'email') $user->assignRole('librarian');
+        else $user->assignRole('user');
+
         event(new Registered($user));
 
         Auth::login($user);
